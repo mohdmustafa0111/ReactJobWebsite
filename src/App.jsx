4eac42a2f6c58ddd324c089bb35b9c81a0e4d570
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-05-09 22:42:37 +0530

    Job Website

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import Joblistings from "../components/Joblistings";
+
+const JobsPage = () => {
+  return (
+    <section className="bg-blue-50 px-4 py-5">
+      <Joblistings />
+    </section>
+  );
+};
+
+export default JobsPage;
